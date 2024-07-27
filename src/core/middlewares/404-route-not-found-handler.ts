87c2b53c84348c546c7378d22c3c990aee673988
Prataad <pondaeroz008y@gmail.com>
2024-07-27 18:42:14 +0700

    update some stuff v2

--- src/core/middlewares/404-route-not-found-handler.ts
+++ src/core/middlewares/404-route-not-found-handler.ts
@@ -1,5 +1,5 @@
-import { BaseResponse } from 'core/base-response.js';
 import { Request, Response } from 'express-serve-static-core';
+import { BaseResponse } from '../base-response.js';
 export const routeNotFoundHandler = (_req: Request, res: Response<BaseResponse<undefined>>) => {
     res.status(404).send({ success: false, message: "404 Route Not Found" });
 }
